6c51f4c1814c44548ed5165b854a6a06b66dba49
65070076 ณัฐนนท์ <70151498+endphaze@users.noreply.github.com>
2024-10-13 15:36:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+import requests
+
 
 app = FastAPI()
 
 
 
 @app.post('/siem_hook')
-def hello_world(payload: dict):
-    return payload
+def hello_world(data: dict):
+    url = "https://discord.com/api/webhooks/1293399469987205151/zyHHdYRLLEG7G9FtdWADwPMUj3NBWbzV1ssWfWW-hlq5MyEKmXwzAkrNivUWMx2aLK2K"
+
+    payload = {
+        "content" : f"{data}"
+    }
+
+    requests.api.post(url, payload)
+
+
 
 
 
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app)
-
