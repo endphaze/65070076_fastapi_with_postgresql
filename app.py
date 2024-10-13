533a550263c62b416c8f1c5e44811b62792e0296
65070076 ณัฐนนท์ <70151498+endphaze@users.noreply.github.com>
2024-10-13 15:49:42 +0700

    Update app.py eiei

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello_world(data: dict):
     url = "https://discord.com/api/webhooks/1293399469987205151/zyHHdYRLLEG7G9FtdWADwPMUj3NBWbzV1ssWfWW-hlq5MyEKmXwzAkrNivUWMx2aLK2K"
 
     payload = {
-        "content" : f"{data}"
+        'content' : f'{data}'
     }
 
     requests.api.post(url, payload)
